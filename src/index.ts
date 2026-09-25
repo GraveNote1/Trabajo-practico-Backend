1cedb6d84d10f7fb1751756fdb9a189f39a1b1c9
GraveNote1 <delvalle.juanjo16@gmail.com>
2026-09-24 22:37:30 -0300

    update added to the code

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,63 @@ const productSchema = new mongoose.Schema<IProduct>({
     stock: {
         type: Number,}
 })
+
+const Product = mongoose.model<IProduct>("Product", productSchema)
+
+const connectDb = async (URI: string) => {
+    try {
+        await mongoose.connect(URI)
+        
+        } 
+    catch (error) {
+        console.error("Error al conectar a MongoDB:", error)
+        process.exit(1)
+    }
+}
+const isValidId = (id: string) => {
+    return mongoose.isValidObjectId(id)
+}
+const getBooks = async (id?: string) => {
+    try {
+        if (!id) {
+            return await Product.find()
+        }
+        if (!isValidId(id)) {
+            throw new Error("Invalid ID format")
+        }
+        const foundBook = await Product.findById(id)
+        if (!foundBook) {
+            throw new Error("Book not found")
+        }
+        return foundBook
+    } catch (error) {
+        console.error("Error al obtener libros:", error)
+        throw error
+    }
+
+}
+const createBook = async (bookData: IProduct) => {
+    try {
+        const newBook = new Product(bookData)
+        return await newBook.save()
+    } catch (error) {
+        console.error("Error al crear libro:", error)
+        throw error
+    }
+}
+const updateBook = async (id: string, bookData: Partial<IProduct>) => {
+    try {
+        if (!isValidId(id)) {
+            throw new Error("Invalid ID format")
+        }
+        const updatedBook = await Product.findByIdAndUpdate(id, bookData, { new: true })
+        if (!updatedBook) {
+            throw new Error("Book not found")
+        }
+        return updatedBook
+    } catch (error) {
+        console.error("Error al actualizar libro:", error)
+        throw error
+    }
+}
+
